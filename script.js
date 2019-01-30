e4f3a47fb851c841d78827ff2df5ebcf8a484c63
Chris Finney <chrisfinney86@gmail.com>
2019-01-30 15:01:03 -0600

    wrote code for playGame function that determines winner

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const computerPlay = () => {
-    const computerInput =  Math.floor(Math.random() * Math.floor(3));
+    const computerInput = Math.floor(Math.random() * Math.floor(3));
     if (computerInput === 0) {
         return 'rock';
     }
@@ -10,8 +10,30 @@ const computerPlay = () => {
         return 'scissors';
     }
 }
+// const playerOne = 'scissors';
+// const playerTwo = 'paper';
 
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
+const humanSelection = 'rock';
+const computerSelection = computerPlay();
+
+const playGame = (human, computer) => {
+    if (human === 'rock' && computer === 'scissors' ||
+        human === 'paper' && computer === 'rock' ||
+        human === 'scissors' && computer === 'paper'
+    ) {
+        return 'Human, you win';
+    }
+    if (human === 'rock' && computer === 'paper' ||
+        human === 'paper' && computer === 'scissors' ||
+        human === 'scissors' && computer === 'rock') {
+        return 'Computer, you win';
+    }
+    if(human === computer){
+        return 'Both of you chose the same thing, but Human you still lose';
+    }
+}
+
+console.log('Human selection ', humanSelection);
+console.log('Computer selection ', computerSelection);
+
+console.log(playGame(humanSelection, computerSelection));
