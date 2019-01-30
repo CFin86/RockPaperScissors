8579ac20acefbc48bda65679c990cf3c50023c58
Chris Finney <chrisfinney86@gmail.com>
2019-01-30 14:21:57 -0600

    solved computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Yes maam');
+const computerPlay = () => {
+    const computerInput =  Math.floor(Math.random() * Math.floor(3));
+    if (computerInput === 0) {
+        return 'rock';
+    }
+    if (computerInput === 1) {
+        return 'paper';
+    }
+    if (computerInput === 2) {
+        return 'scissors';
+    }
+}
+
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
